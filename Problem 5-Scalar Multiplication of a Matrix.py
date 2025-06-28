cfb2b2f4b94bb2ea9f0b3f514a11ce24d1a10d58
Naman Raiyani <121411544+namanraiyani@users.noreply.github.com>
2025-06-28 16:29:47 +0530

    Delete Problem 5-Scalar Multiplication of a Matrix.py

--- Problem 5-Scalar Multiplication of a Matrix.py
+++
@@ -1,5 +0,0 @@
-# https://www.deep-ml.com/problems/5
-def scalar_multiply(matrix: list[list[int|float]], scalar: int|float) -> list[list[int|float]]:
-    a,b,c,d = matrix[0][0],matrix[0][1],matrix[1][0],matrix[1][1]
-    result = [[a*scalar, b*scalar], [c*scalar, d*scalar]]
-	return result
