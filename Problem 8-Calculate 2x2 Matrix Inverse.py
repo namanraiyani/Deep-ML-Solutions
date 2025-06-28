6b360c4e49c4381b7edde2c537220205adb8bbf1
Naman Raiyani <121411544+namanraiyani@users.noreply.github.com>
2025-06-28 16:29:40 +0530

    Delete Problem 8-Calculate 2x2 Matrix Inverse.py

--- Problem 8-Calculate 2x2 Matrix Inverse.py
+++
@@ -1,5 +0,0 @@
-# https://www.deep-ml.com/problems/8
-def inverse_2x2(matrix: list[list[float]]) -> list[list[float]]:
-    det = matrix[0][0]*matrix[1][1] - matrix[0][1]*matrix[1][0]
-    inverse = [[matrix[1][1]/det, -matrix[0][1]/det]], [[-matrix[1][0]/det, matrix[0][0]/det]]
-	return inverse
