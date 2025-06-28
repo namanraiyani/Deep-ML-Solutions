ac72db9141a8a986c3803dc031b828426270e1ba
Naman Raiyani <121411544+namanraiyani@users.noreply.github.com>
2025-06-28 16:28:22 +0530

    Delete Problem 121-Vector Element-wise Sum.py

--- Problem 121-Vector Element-wise Sum.py
+++
@@ -1,7 +0,0 @@
-# https://www.deep-ml.com/problems/121
-def vector_sum(a: list[int|float], b: list[int|float]) -> list[int|float]:
-	# Return the element-wise sum of vectors 'a' and 'b'.
-	# If vectors have different lengths, return -1.
-	if len(a) != len(b):
-        return -1
-    return [x + y for x, y in zip(a, b)]
