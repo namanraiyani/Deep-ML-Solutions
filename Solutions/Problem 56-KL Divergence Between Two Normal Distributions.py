b2f29a19939bc2420543396266c6a77beb766356
Naman Raiyani <121411544+namanraiyani@users.noreply.github.com>
2025-08-10 17:36:55 +0530

    Create Problem 56-KL Divergence Between Two Normal Distributions.py

---
+++ Solutions/Problem 56-KL Divergence Between Two Normal Distributions.py
@@ -0,0 +1,6 @@
+# https://www.deep-ml.com/problems/56
+
+import numpy as np
+
+def kl_divergence_normal(mu_p, sigma_p, mu_q, sigma_q):
+	return np.log(sigma_q/sigma_p) + ((sigma_p**2 + (mu_p-mu_q)**2)/(2*(sigma_q**2))) - 0.5
